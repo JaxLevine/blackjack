f68d9b5c5961583dcde6ee097b5a05fa7f11aac4
JaxLevine <jacksonlevine@yahoo.com>
2024-02-26 13:10:34 -0500

    shuffled deck, dealt cards, created more state variables

--- js/main.js
+++ js/main.js
@@ -1,21 +1,16 @@
 /*----- constants -----*/
 
-
 /*----- app's state (variables) -----*/
-let deck;
-
-let dealerHand;
-let playerHand;
-let dealerTotal;
-let playerTotal;
-
-
+// let deck;
+// let dealerHand;
+// let playerHand;
+// let dealerTotal;
+// let playerTotal;
+let shuffledDeck = shuffleDeck(deck);
+let hands = dealCards(deck)
 /*----- cached element references -----*/
 
-
-
 /*----- event listeners -----*/
-
 
 /*----- functions -----*/
 
@@ -23,17 +18,35 @@ function createDeck() {
   const cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
   const suits = ['H', 'S', 'D', 'C'];
   let deck = [];
-  
+
   for (let x = 0; x < cards.length; x++) {
     for (let y = 0; y < suits.length; y++) {
-      deck.push(`${cards[x]} of ${suits[y]}`);
+      deck.push(`${cards[x]}-${suits[y]}`);
     }
   }
   return deck;
 }
 
-console.log(createDeck())
-
-function shuffleDeck() {
-  
+function shuffleDeck(deck) {
+  for (let x = 0; x < deck.length; x++) {
+    let randomNum = Math.floor(Math.random() * deck.length);
+    let currentCard = deck[x];
+    deck[x] = deck[randomNum];
+    deck[randomNum] = currentCard;
+  }
+  console.log(`Shuffled Deck: ${deck}`);
 }
+
+//more game variables//
+let playerHand = [];
+let dealerHand = [];
+let deck = shuffleDeck(createDeck());
+
+function dealCards () {
+  playerHand.push(deck.pop(), deck.pop());
+  dealerHand.push(deck.pop(), deck.pop());
+  update();
+}
+
+
+
