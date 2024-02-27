bbe9a14c4d1ba799a9c1b20be37f3216a57ac8cd
JaxLevine <jacksonlevine@yahoo.com>
2024-02-27 14:02:01 -0500

    Changed a lot. Renamed variables, re-organized, added player action and displayed results

--- js/main.js
+++ js/main.js
@@ -1,24 +1,32 @@
-/*----- constants -----*/
+//----- constants ----- //
+const cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
+const suits = ['H', 'S', 'D', 'C'];
 
-/*----- app's state (variables) -----*/
+//----- app's state (variables) ----- //
 let completeDeck = createDeck(); // Full and proper deck
 let shuffledDeck = shuffleDeck(completeDeck); // Full deck shuffled
-let dealtHands = dealCards() //Hands for player and dealer (that have been dealt)
+let dealtHands = dealCards(); // Hands for player and dealer (that have been dealt)
+let playerHand = dealtHands.playerHand; // Global player hand
+let dealerHand = dealtHands.dealerHand; // Global dealer hand
 
-/*----- cached element references -----*/
+//----- cached element references ----- //
+let dealerFaceUpCard = document.querySelector('#faceUp');
+let dealerFaceDown = document.querySelector('#faceDown');
+let playerCardOne = document.querySelector('#playerCardOne');
+let playerCardTwo = document.querySelector('#playerCardTwo');
+let hitAction = document.querySelector('#hitButton');
+let standAction = document.querySelector('#stayButton');
 
-/*----- event listeners -----*/
+//----- event listeners ----- //
+hitAction.addEventListener('click', playerHits);
+standAction.addEventListener('click', playerStands);
 
-/*----- functions -----*/
 
+//--------------------------- Create Deck -----------------------------//
 
-
-//Create the deck//
+// Create // 
 function createDeck() {
-  const cards = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A'];
-  const suits = ['H', 'S', 'D', 'C'];
   let completeDeck = [];
-
   for (let x = 0; x < cards.length; x++) {
     for (let y = 0; y < suits.length; y++) {
       completeDeck.push(`${cards[x]}-${suits[y]}`);
@@ -27,9 +35,9 @@ function createDeck() {
   return completeDeck;
 }
 
+//--------------------------- Shuffle Deck -----------------------------//
 
-
-//Shuffle the deck (let shuffledDeck)//
+// Shuffle //
 function shuffleDeck(completeDeck) {
   for (let x = 0; x < completeDeck.length; x++) {
     let randomNum = Math.floor(Math.random() * completeDeck.length);
@@ -41,32 +49,69 @@ function shuffleDeck(completeDeck) {
   return completeDeck;
 }
 
+//-------------------------- Deal and Display Cards ---------------------------//
 
-
-//Dealing cards (let dealtCards)//
+// Deal //
 function dealCards() {
-  let playerHand = [completeDeck.pop(), completeDeck.pop()];
-  let dealerHand = [completeDeck.pop(), completeDeck.pop()];
-  return { playerHand, dealerHand };
+  let playerHand = [shuffledDeck.pop(), shuffledDeck.pop()];
+  let dealerHand = [shuffledDeck.pop(), shuffledDeck.pop()];
+  return {playerHand, dealerHand};
 }
 
+// Display //
+function displayCards(playerHand, dealerHand, dontRevealDealer = false) {
+  console.log(`Dealer's Hand: ${dealerHand[0]}, [hidden]`);
+  console.log(`Player's Hand: ${playerHand[0]}, ${playerHand[1]}`);
+  dealerFaceUpCard.src = `./cards/${dealerHand[1]}.png`;
+  if (!dontRevealDealer) {
+    dealerFaceDown.src = `./cards/BACK.png`;
+  }
+  playerCardOne.src = `./cards/${playerHand[0]}.png`;
+  playerCardTwo.src = `./cards/${playerHand[1]}.png`;
+} 
 
+//--------------------------- Player Action -----------------------------//
 
-//Displaying cards//
-function displayCards(playerHand, dealerHand) {
-  console.log(`Dealer's Hand: ${dealerHand[0]}, [hidden]`)
-  console.log(`Player's Hand: ${playerHand[0]}, ${playerHand[1]}]`)
-} displayCards(dealtHands.playerHand, dealtHands.dealerHand);
+// Hit //
+function playerHits() {
+  let newCard = shuffledDeck.pop(); // Gets the new card from the deck
+  playerHand.push(newCard); // Adds new card to player's hand
+  //  create new <img> element for the new card and append it
+  const newCardPng = document.createElement('img');
+  newCardPng.src = `./cards/${newCard}.png`; // Set the source of the new card image
+  newCardPng.style.height = '275px';
+  newCardPng.style.margin = '4px';
+  document.querySelector('.player-cards').appendChild(newCardPng); // Appends as a child to player cards class
+  
+  if (calculateScore(playerHand) > 21) {   // See if player busted after the hit
+    console.log('Sorry Brother, You Busted.');
+  }
+}
 
+// Stand //
+function playerStands() {
+  dealerMove();
+}
 
+//--------------------------- Dealer Action -----------------------------//
 
-//Calculate player's score//
-function calculatePlayerScore(playerHand) {
+//Dealer Move
+function dealerMove() {
+  while (calculateScore(dealerHand) < 17) {
+    dealerHand.push(shuffledDeck.pop());
+  }
+  dealerFaceDown.src = `./cards/${dealerHand[0]}.png`; //reveals facdown card now
+  checkForWinner();
+}
+
+//--------------------------- Calculations -----------------------------//
+
+// Calculate Scores //
+function calculateScore(hand) {
   let score = 0;
   let aces = 0;
 
-  for (let x = 0; x < playerHand.length; x++) {
-    let card = playerHand[x];
+  hand.forEach(function (card) {
     let cardValue = card.split('-')[0];
     if (cardValue === 'A') {
       aces += 1;
@@ -76,58 +121,41 @@ function calculatePlayerScore(playerHand) {
     } else {
       score += parseInt(cardValue, 10);
     }
-  }
-}
+  });
 
-
-
-//Calculate dealer's score//
-function calculateDealerScore(dealerHand) {
-  let score = 0;
-  let aces = 0;
-
-  for (let x = 0; x < dealerHand.length; x++) {
-    let card = dealerHand[x];
-    let cardValue = card.split('-')[0];
-    if (cardValue === 'A') {
-      aces += 1;
-      score += 11;
-    } else if (cardValue === 'J' || cardValue === 'Q' || cardValue === 'K') {
-      score += 10;
-    } else {
-      score += parseInt(cardValue, 10);
-    }
-  }
-  return score;
-}
-
-
-
-//Aces caveat//
-function adjustScoreForAces() {
   while (score > 21 && aces > 0) {
     score -= 10;
     aces -= 1;
   }
+
   return score;
 }
 
+//--------------------------- Determining Winner -----------------------------//
 
+// Checking Winner //
+function checkForWinner() {
+  let playerScore = calculateScore(playerHand);
+  let dealerScore = calculateScore(dealerHand);
 
+  if (playerScore > 21) {
+    console.log('You Busted.');
+  } else if (dealerScore > 21) {
+    console.log('Dealer Busts. You Win!');
+  } else if (playerScore > dealerScore) {
+    console.log('You Win!');
+  } else if (playerScore < dealerScore) {
+    console.log('You Lose!');
+  } else {
+    console.log('Push.');
+  }
+}
 
+//--------------------------- Initialize Game -----------------------------//
 
+// Start Game //
+function init() {
+  displayCards(playerHand, dealerHand);
+}
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+init();
